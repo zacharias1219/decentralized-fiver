b1c67eefff5b1700e39454da3da0e8f043355557
Richard Abishai <rickoshade1891@gmail.com>
2024-07-09 11:45:24 +0530

    initial

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,8 @@
 import express from "express";
+import userRouter from "./routers/user"
+import workerRouter from "./routers/worker"
 
 const app = express();
 
+app.use("/v1/user", userRouter);
+app.use("/v1/worker", workerRouter);

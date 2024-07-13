10a5bdd7a846be277a9b3240f7d1e13cba423767
Richard Abishai <rickoshade1891@gmail.com>
2024-07-13 20:30:52 +0530

    chore: Update URLs for backend and cloudfront

--- user-frontend/components/UploadImage.tsx
+++ user-frontend/components/UploadImage.tsx
@@ -49,7 +49,7 @@ export function UploadImage({ onImageAdded, image }: {
                     <div className="h-full flex justify-center w-full pt-16 text-4xl">
                     {uploading ? <div className="text-sm">Loading...</div> : <>
                         +
-                        <input className="w-full h-full bg-red-400 w-40 h-40" type="file" style={{position: "absolute", opacity: 0, top: 0, left: 0, bottom: 0, right: 0, width: "100%", height: "100%"}} onChange={onFileSelect} />
+                        <input className="w-full h-full bg-red-400" type="file" style={{position: "absolute", opacity: 0, top: 0, left: 0, bottom: 0, right: 0, width: "100%", height: "100%"}} onChange={onFileSelect} />
                     </>}
                 </div>
             </div>
